3c22fae5e054b38af6a31270bc90fa3636689217
Piotr <piotr.romatowski@gmail.com>
2020-01-31 18:12:59 +0100

    js-sprawdzanie-liczby2

---
+++ main.js
@@ -0,0 +1,13 @@
+let x = 3
+let y= 17
+
+
+if (x == 20) {
+  console.log ('TRUE') ;
+} else if (y == 20) {
+  console.log ('TRUE');
+} else if (x + y <= 20) {
+   console.log  ('TRUE');
+} else {
+     console.log ('FALSE')
+};
