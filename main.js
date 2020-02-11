d959e7cca1c76eb7a0710e250f01d118ce346790
Piotr <piotr.romatowski@gmail.com>
2020-02-11 18:01:45 +0100

    js-sprawdzanie-liczby2

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ let x = 3
 let y= 17
 
 
-if (x == 20 || y ==20 || x + y ==20) {
+function sprawdzenie (x,y) {
+  if (x == 20 || y ==20 || x + y ==20) {
   return true ;
-} else {
+  } else {
      return false;
+  }
 };
+
+sprawdzenie(x,y);
